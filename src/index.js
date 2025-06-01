0503d66f1e8bbdd4c6d3bec1749f7b20f83a1d20
Arya Gupta <btech10759@bitmesra.ac.in>
2025-06-01 10:33:08 +0530

    fix:correct axiosConfig import path Casing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./Context/AuthContext";
 import App from "./App";
-import "./utils/axiosConfig";
+import "./Utils/axiosConfig";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
